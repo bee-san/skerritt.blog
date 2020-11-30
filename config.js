a0ab5307a41cad6600e6925eb20e6b5a7ea39aac
brandon <brandonskerritt51@gmail.com>
2020-11-30 01:51:27 +0000

    finished pp2

--- config.js
+++ config.js
@@ -8,8 +8,8 @@ module.exports = {
   copyright: '© All rights reserved.',
   disqusShortname: '',
   postsPerPage: 500,
-  googleAnalyticsId: 'UA-73379983-2',
-  useKatex: false,
+  googleAnalyticsId: '',
+  useKatex: true,
   menu: [
     {
       label: 'Articles',
